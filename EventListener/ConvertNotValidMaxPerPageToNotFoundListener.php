fb2ec2f64dbf5f4b213676e65c8f64a030696c18
Alexander Pysarchuk <then.bemyguest@gmail.com>
2020-03-26 13:57:56 +0200

    Update exception event

--- EventListener/ConvertNotValidMaxPerPageToNotFoundListener.php
+++ EventListener/ConvertNotValidMaxPerPageToNotFoundListener.php
@@ -3,31 +3,25 @@ namespace  WhiteOctober\PagerfantaBundle\EventListener;
 
 use Pagerfanta\Exception\NotValidMaxPerPageException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ConvertNotValidMaxPerPageToNotFoundListener implements EventSubscriberInterface
 {
     /**
-     * @param GetResponseForExceptionEvent $event
+     * @param ExceptionEvent $event
      */
-    public function onException(GetResponseForExceptionEvent $event)
+    public function onException(ExceptionEvent $event)
     {
         if (method_exists($event, 'getThrowable')) {
             $throwable = $event->getThrowable();
-        } else {
-            // Support for Symfony 4.3 and before
-            $throwable = $event->getException();
         }
 
         if ($throwable instanceof NotValidMaxPerPageException) {
             $notFoundHttpException = new NotFoundHttpException('Page Not Found', $throwable);
             if (method_exists($event, 'setThrowable')) {
                 $event->setThrowable($notFoundHttpException);
-            } else {
-                // Support for Symfony 4.3 and before
-                $event->setException($notFoundHttpException);
             }
         }
     }
